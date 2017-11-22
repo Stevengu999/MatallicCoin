d0d9fb5f388b284a54a0471974930d1cb48876e3
iketheadore <2589926+iketheadore@users.noreply.github.com>
2017-11-23 00:20:14 +0800

    Update gui static files

--- src/util/droplet/droplet_test.go
+++ src/util/droplet/droplet_test.go
@@ -72,8 +72,8 @@ func TestFromString(t *testing.T) {
 			n: 123000456,
 		},
 		{
-			s: "100SKY",
-			e: errors.New("can't convert 100SKY to decimal"),
+			s: "100MTC",
+			e: errors.New("can't convert 100MTC to decimal"),
 		},
 		{
 			s: "",
